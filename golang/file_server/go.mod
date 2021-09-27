364754d9f0a442f4100d57a6e61680fde51de07b
耿洪杰 <jie.geng@jitstack.com>
2021-09-27 14:10:28 +0800

    文件服务器

---
+++ golang/file_server/go.mod
@@ -0,0 +1,3 @@
+module genghongjie/go-code/golang/file_server
+
+go 1.16
--- golang/file_server/main.go
+++ golang/file_server/main.go
@@ -2,23 +2,18 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
 	"net/http"
-
-	"github.com/prometheus/common/log"
 )
 
 func main() {
 	dir := flag.String("dir", "/tmp", "默认路径")
 
-	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir(*dir))
+
+	mux := http.NewServeMux()
 	mux.Handle("/", fs)
-	fmt.Println("文件服务启动 文件路径为 ", dir)
+
+	log.Println("文件服务启动 文件路径为 ", *dir)
 	log.Fatalln(http.ListenAndServe(":10000", mux))
 }
-
-//func main() {
-//	http.Handle("/", http.FileServer(http.Dir("./")))
-//	http.ListenAndServe(":8080", nil)
-//}

74eb678e53837160a5d68ead30900b783e0953ec
耿洪杰 <hank.geng@jitstack.com>
2022-02-17 10:59:43 +0800

    优化文件服务

--- golang/file_server/main.go
+++ golang/file_server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"runtime/pprof"
@@ -10,6 +13,7 @@ import (
 
 func main() {
 	dir := flag.String("dir", "./", "默认路径")
+	port := flag.String("port", "10000", "启动端口")
 	cpuProfile := flag.String("cpu_profile", "", "cpu 分析写入到指定文件，可通过 go tool prop /tmp/cpufile 进行分析")
 
 	flag.Parse()
@@ -23,7 +27,14 @@ func main() {
 	mux.Handle("/", fs)
 
 	log.Println("文件服务启动 文件路径为 ", *dir)
-	log.Fatalln(http.ListenAndServe(":10000", mux))
+	log.Println("访问路径微 http://127.0.0.1:" + *port)
+
+	ip, err := externalIP()
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Println("访问路径微 http://" + ip.String() + ":" + *port)
+	log.Fatalln(http.ListenAndServe(":"+*port, mux))
 }
 
 //性能分析开关
@@ -36,4 +47,50 @@ func performanceProfiling(cpuProfile *string) {
 		_ = pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
+}
+
+func externalIP() (net.IP, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		for _, addr := range addrs {
+			ip := getIpFromAddr(addr)
+			if ip == nil {
+				continue
+			}
+			return ip, nil
+		}
+	}
+	return nil, errors.New("connected to the network?")
+}
+
+func getIpFromAddr(addr net.Addr) net.IP {
+	var ip net.IP
+	switch v := addr.(type) {
+	case *net.IPNet:
+		ip = v.IP
+	case *net.IPAddr:
+		ip = v.IP
+	}
+	if ip == nil || ip.IsLoopback() {
+		return nil
+	}
+	ip = ip.To4()
+	if ip == nil {
+		return nil // not an ipv4 address
+	}
+
+	return ip
 }
